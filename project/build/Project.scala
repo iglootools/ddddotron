18bbfcc1767eea7a9a536419de5feb36777e767a
Sami Dalouche <sami.dalouche@gmail.com>
2011-05-17 14:49:40 -0400

    moved JodaTimeSerializer to iglootools-commons

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -20,10 +20,10 @@ class Project(info: ProjectInfo) extends ParentProject(info) with IdeaProject  {
     val LiftJsonVersion = "2.3"
     val CamelVersion = "2.7.1"
     val ActiveMqVersion = "5.5.0"
+    val IglootoolsCommonsVersion = "0.2"
 
     object Test {
       lazy val junit = "junit" % "junit" % Dependencies.JunitVersion % "test" withSources()
-      //lazy val junitInterface = "com.novocode" % "junit-interface" % Dependencies.JunitInterfaceVersion % "test->default"
       lazy val scalaTest = "org.scalatest" % "scalatest" % "1.2"  % "test" withSources()
       lazy val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test" withSources()
       lazy val logbackTest = "ch.qos.logback" % "logback-classic" % Dependencies.LogbackVersion % "test" // withSources()
@@ -49,8 +49,10 @@ class Project(info: ProjectInfo) extends ParentProject(info) with IdeaProject  {
     lazy val jodaTime = "joda-time" % "joda-time" % "1.6.2" withSources()
     lazy val scalaTime = "org.scala-tools.time" %% "time" % "0.3" // withSources()
     lazy val scalaj_collection = "org.scalaj" % "scalaj-collection_2.8.0" % "1.0"
-    lazy val iglootoolsCommons = "org.iglootools.commons" %% "iglootools-commons-scala" % "0.1"
+    lazy val iglootoolsCommonsScalaLang = "org.iglootools.commons" %% "iglootools-commons-scala-lang" % Dependencies.IglootoolsCommonsVersion
 
+    lazy val liftJson = "net.liftweb" %% "lift-json" % Dependencies.LiftJsonVersion withSources()
+    lazy val liftJsonExt = "net.liftweb" %% "lift-json-ext" % Dependencies.LiftJsonVersion withSources()
 
     // test
     lazy val junit = Dependencies.Test.junit
@@ -63,8 +65,8 @@ class Project(info: ProjectInfo) extends ParentProject(info) with IdeaProject  {
   class EventStore(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
     override def ivyXML = Dependencies.ivyXML
 
-    lazy val liftJson = "net.liftweb" %% "lift-json" % Dependencies.LiftJsonVersion withSources()
-    lazy val liftJsonExt = "net.liftweb" %% "lift-json-ext" % Dependencies.LiftJsonVersion withSources()
+    lazy val iglootoolsCommonsScalaLang = "org.iglootools.commons" %% "iglootools-commons-ext" % Dependencies.IglootoolsCommonsVersion
+
     lazy val springJdbc = "org.springframework" % "spring-jdbc" % Dependencies.SpringFrameworkVersion withSources()
     lazy val springContextSupport = "org.springframework" % "spring-context-support" % Dependencies.SpringFrameworkVersion withSources()
     lazy val hsqldb = "org.hsqldb" % "hsqldb" % "2.0.0" //withSources()
